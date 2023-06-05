5625fcfbebe4d6e8c8bd1c5cd71f9186a886e5e0
minechoo <choominhae@gmail.com>
2023-06-05 13:37:44 +0900

    주석

--- js/department.js
+++ js/department.js
@@ -7,6 +7,31 @@ const wrap = document.querySelector('.department .wrap');
 // 	console.log(data);
 // }
 
+/*
+fetch('데이터 URL)
+.then((data)=> data.json()).then.((json)=>{
+  //데이터가 성공적으로 받아졌을때 실행할 구문
+})
+.catch(()=>{
+  //실패했을때 구문
+})
+*/
+
+//외부 데이터 가져오는 공식
+/*
+fetch('데이터 url').then((data) => {
+  data.json()
+     .then((json) => {
+        //데이터가 성공적으로 받아졌을 떄 구문
+        console.log(json);
+     })
+     .catch((err) => {
+        //응답에 실패했을 때 실행되는 구문
+        console.log(err);
+     });
+});
+*/
+
 let tags = '';
 
 fetch('/DB/department.json')
